e76d608140f0b9b6b39973862169d9b4caf6e27d
jmqp7373 <jmqp7373@gmail.com>
2025-10-13 22:49:14 -0500

    chore: Agregar vendor para deployment en hosting compartido

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b52952d71f2abf031db4a2ec8101180168342a1a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b52952d71f2abf031db4a2ec8101180168342a1a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '8.8.3',
+            'version' => '8.8.3.0',
+            'reference' => 'a2db2ea514498c4c55de1dfe5b858c8fd3f728ab',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
